148061f75b1c79303ce0b8b92b64bfa5373650eb
HannaBM12 <Hannabmulugeta@gmail.com>
2021-03-25 16:19:40 -0400

    commit tree 0d701731e0137779f8b8302545e49346d6266e22

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(array, event){
+    let thankCard = []
+    for (let i=0; i<array.length; i++){
+  thankCard.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)}
+  return thankCard;
+}
+
+function countDown(num){
+    let i = num;
+    while (i > 0){
+        console.log(i);
+        i = i - 1;
+        }
+    console.log(i);
+}
